b6a45ad496280cef1da705704cfebb3c72404b85
ycurbill <108531077+ycurbill@users.noreply.github.com>
2022-12-27 14:16:14 +0100

    init repo

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Nicolas Tagliafico',
+        username: 'Nico',
+        email: 'nicotaglia@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Castello Lukeba',
+        username: 'Castello',
+        email: 'castellojr@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Alexandre Lacazette',
+        username: 'Lacaz',
+        email: 'alexlacazzz@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Malo Gusto',
+        username: 'Malo',
+        email: 'malogusto@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Corentin Tolisso',
+        username: 'Coco',
+        email: 'cocotolisso@gmail.com'
+    }
+];
